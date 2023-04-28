252092ff475891d506a43009cfdee6cc9f5e8ac9
Skay <skai-zhang@hotmail.com>
2023-04-28 21:00:00 +0800

    fix bug

--- README.md
+++ README.md
@@ -12,3 +12,24 @@ This is a tool to find the installation location of an application
 
 * [x] GBK
 * [x] UTF8
+
+## How Use
+
+### Add to project
+```shell
+# Use npm
+npm add find-install-path
+# Use pnpm
+pnpm add find-install-path
+# Use yarn
+yarn add find-install-path
+```
+
+### Code Examples
+
+```javascript
+// Import
+import { find } from 'find-install-path';
+// Use
+const path = find('Microsoft Edge');
+```
---
+++ lib/index.d.ts
@@ -0,0 +1,10 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License.
+ *  REQUIREMENT: This definition is dependent on the @types/node definition.
+ *  Install with `npm install @types/node --save-dev`
+ *--------------------------------------------------------------------------------------------*/
+
+declare module 'find-install-path' {
+	export function find(name: string): string;
+}
