dd3583bedc8036ec8fcabdba302cc2e227b8588b
Skay <skai-zhang@hotmail.com>
2023-04-28 21:06:25 +0800

    fix test bug

--- test/test.js
+++ test/test.js
@@ -1,8 +1,8 @@
-import { findInstallPath } from '../lib/index.js';
+const findInstallPath = require('../lib/index.js');
 
 findEdgePath();
 function findEdgePath() {
-    findInstallPath('Microsoft Edge').then(res => {
+    findInstallPath.find('Microsoft Edge').then(res => {
         console.log('Edge is installed on ' + res);
     }).catch(err=>{
         // What should you do?
